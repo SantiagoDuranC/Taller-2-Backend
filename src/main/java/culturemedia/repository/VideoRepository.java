853553b3e9e1d4c40deb6d4391a6627e8774b139
Santiago Durán Carmona <santi.dc555@gmail.com>
2024-04-20 02:37:27 -0500

    Adding video structures

--- src/main/java/culturemedia/repository/VideoRepository.java
+++ src/main/java/culturemedia/repository/VideoRepository.java
@@ -7,7 +7,7 @@ import culturemedia.model.Video;
 public interface VideoRepository
 {
     List<Video> findAll();
-    Video add(Video save);
+    Video add(Video video);
     List<Video> find(String title);
     List<Video> find(Double fromDuration, Double toDuration);
 }
